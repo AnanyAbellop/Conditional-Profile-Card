c4c464ed6abbcb2ef9aac43d90df0cec34c2199b
AnanyAbellop <padronalbany@gmail.com>
2020-07-17 01:19:36 +0000

    Ya todo sirve

--- src/js/index.js
+++ src/js/index.js
@@ -34,7 +34,8 @@ function render(variables = {}) {
   if (variables.city == false) {
     if (variables.country == false) location = "<h3></h3>";
   }
-
+  let posicion = `<ul class="${variables.socialMediaPosition}">`;
+  if (posicion == false) posicion = '<ul class="position-right">';
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
@@ -42,11 +43,11 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastname}</h1>
           ${roll}
           ${location}
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          ${posicion}
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -66,8 +67,8 @@ window.onload = function() {
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
-    github: "alesanchezr",
+    twitter: "",
+    github: "",
     linkedin: "",
     instagram: "",
     name: "Name",
